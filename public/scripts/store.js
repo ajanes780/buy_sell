9c0dc6b2aea8ee33e91290b5db51055741e59a02
ajanes780 <aaron.janes@gmail.com>
2021-02-09 23:36:21 +0000

    make comments to describe functions

--- public/scripts/store.js
+++ public/scripts/store.js
@@ -1,20 +1,19 @@
-// const { response } = require("express");
-
-// const { response } = require("express");
-
+// store ajax api requests
 $(() => {
   console.log(" JQ loaded ");
-
+  // make a request to the api for info  and return a promise called response
   $.ajax({
     method: "GET",
     url: "/api/products",
   }).then((response) => {
+    // create a loop that goes through the images variable and emptys each container and then adds new
+    // html and a on click listener
     let images = response.products;
-    console.log("this is images", images);
     let y = 1;
     for (let obj of images) {
       $(`#item-${y}`).empty();
       $(`#item-${y}`).html(` <img src="${obj.picture_url}"> </a>`);
+      // on click of a store item we empty the container and reload with our single product markup variable
       $(`#item-${y}`).click(function () {
         $(".saleitem-title").empty();
         const markup = `<div class="single-product-markup">
@@ -32,43 +31,15 @@ $(() => {
     />
     </div>
     <div class= "icongroup">
-    <i class="fas fa-flag"></i>
+    <i class="far fa-envelope"></i>
     <i class="fas fa-heart"></i>
-     </div>
+    </div>
     </div>
     `;
+        // this will add the mark up to the sale item container
         $(".saleitem-title").prepend(markup);
       });
       y += 1;
     }
   });
 });
-
-// function loadsingleproduct(obj){
-
-// const markup = `<div class="single-product-markup">
-// <div>
-//   <h2>${obj.products.name}</h2>
-//   <div>
-//       <h4> Product Description </h4>
-//       <h3>
-//       ${obj.products.description}
-//       </h3>
-// </div>
-
-// <img
-//   src=${obj.products.picture_url}
-// />
-// </div>
-// <footer>
-//     <div class= "icongroup">
-//     <i class="fas fa-flag"></i>
-//     <i class="fas fa-retweet"></i>
-//     <i class="fas fa-heart"></i>
-//     </div>
-//     </footer>
-// </div>
-// `
-// return markup
-
-// };
