e7c9b5f967c63718be3b03e950bf0e17bd6f4d49
ajanes780 <aaron.janes@gmail.com>
2021-02-10 18:51:31 +0000

    add ajaxand markup to feature items

--- public/scripts/store.js
+++ public/scripts/store.js
@@ -1,48 +1,47 @@
-// store ajax api requests
 $(() => {
-  console.log(" JQ loaded ");
-
-  // make a request to the api for info  and return a promise called response
   $.ajax({
     method: "GET",
     url: "/api/products",
   }).then((response) => {
-    // create a loop that goes through the images variable and emptys each container and then adds new
-    // html and a on click listener
     let images = response.products;
     let y = 1;
-    for (let obj of images) {
+    let productId = [];
+    let objArray = [];
+
+    while (productId.length < 9) {
+      let number = Math.floor(Math.random() * images.length);
+      if (productId.indexOf(number) === -1) {
+        productId.push(number) && objArray.push(images[number]);
+      }
+    }
+
+    for (let obj of objArray) {
       $(`#item-${y}`).empty();
       $(`#item-${y}`).html(` <img src="${obj.picture_url}"> </a>`);
-      // on click of a store item we empty the container and reload with our single product markup variable
       $(`#item-${y}`).click(function () {
-        $(".saleitem-title").empty();
         const markup = `<div class="single-product-markup">
-    <div>
-      <h2>${obj.name}</h2>
-      <div>
-          <h4> Product Description </h4>
-          <h3>
-          ${obj.description}
-          </h3>
-    </div>
+        <div>
+          <h2>${obj.name}</h2>
+          <div>
+              <h4> Product Description </h4>
+              <h3>
+              ${obj.description}
+              </h3>
+        </div>
 
-    <img
-      src=${obj.picture_url}
-    />
-    </div>
-    <div class= "icongroup">
-    <button class="email" > Email Me </button>
-    <button class="textme" > Text Me </button>
-    <button class="fav" > Favorite </button>
-    <i class="far fa-envelope"></i>
-    <i class="fas fa-sms"></i>
-    <i class="fas fa-heart"></i>
-    </div>
-    </div>
-
-    `;
-        // this will add the mark up to the sale item container
+        <img
+          src=${obj.picture_url}
+        />
+        </div>
+        <div class= "icongroup">
+        <button class="email" > Email Me </button>
+        <button class="textme" > Text Me </button>
+        <button class="fav" > Favorite </button>
+        <i class="far fa-envelope"></i>
+        <i class="fas fa-sms"></i>
+        <i class="fas fa-heart"></i>
+        </div>
+        `;
         $(".saleitem-title").prepend(markup);
         $(".email").on("click", function () {
           window.location.href =
@@ -55,11 +54,11 @@ $(() => {
         });
         $(".textme").on("click", function () {
           window.location.href =
-            "mailto:user@example.com?subject= I would like to buy your item &body=how low will you go ? ";
+            "mailto:user@example.com?subject= I would like to buy your item &body=how low will you go? ";
           console.log(" you clicked me");
         });
       });
-      y += 1;
+      y++;
     }
   });
 });
